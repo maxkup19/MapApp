6cfca5c013fc349148ab03894e036bb3e43984cd
Maksym Kupchenko <maxkup19@gmail.com>
2022-08-16 23:01:23 +0200

    Add Map to View and map updating logic

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -6,12 +6,34 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
+    //All loaded locations
     @Published var locations: [Location]
+    
+    //Current Location
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation {
+            mapRegion = MKCoordinateRegion(center: location.coordinates,
+                                           span: mapSpan)
+        }
     }
 }
--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -11,14 +11,13 @@ import MapKit
 struct LocationsView: View {
     
     @EnvironmentObject private var lvm: LocationsViewModel
-    @State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
-        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $mapRegion)
+            Map(coordinateRegion: $lvm.mapRegion)
                 .ignoresSafeArea()
+            
+            
         }
     }
 }
