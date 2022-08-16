fe5c2063ab9c070853ca393dea3fdc199f4c498f
Maksym Kupchenko <maxkup19@gmail.com>
2022-08-16 22:18:14 +0200

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Max Kup on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
